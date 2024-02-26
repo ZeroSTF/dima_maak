a9747ee18921812b06214a373d34f5dc94888fa2
ZeroSTF <kateskatous@gmail.com>
2024-02-27 00:54:40 +0100

    security changes

--- src/main/java/tn/esprit/dima_maak/config/SecurityConfig.java
+++ src/main/java/tn/esprit/dima_maak/config/SecurityConfig.java
@@ -2,21 +2,35 @@ package tn.esprit.dima_maak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    @Bean
+    InMemoryUserDetailsManager users(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("riadh.chnitir@esprit.tn")
+                        .password("{noop}password")
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/secured").permitAll();
+                    auth.requestMatchers("/home_page_todo").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .oauth2Login(withDefaults())
--- src/main/java/tn/esprit/dima_maak/controllers/HomeController.java
+++ src/main/java/tn/esprit/dima_maak/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package tn.esprit.dima_maak.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "home test";
+        return "hello user";
     }
 
-    @GetMapping("/secured")
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
     public String secured(){
-        return "secured test";
+        return "hello admin";
     }
 }
