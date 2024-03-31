10e37d8a408f6235fb34614547ba430fe1eae539
RamiLaabidi <rami.laabidi@espri.tn>
2024-03-31 22:04:39 +0100

    V0

---
+++ src/main/java/tn/esprit/dima_maak/services/IEmailServices.java
@@ -0,0 +1,9 @@
+package tn.esprit.dima_maak.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IEmailServices {
+
+
+    String sendMail(MultipartFile[] file, String to, String[] cc, String subject, String body);
+}
