ea8ae1e911eb2504638bd3e8f0fbe3ac5a133d87
ZeroSTF <kateskatous@gmail.com>
2024-03-31 05:00:31 +0100

    FIXED LOGIN!!! (using Autowired and Lazy annotations instead of AllArgsConstructor)

--- src/main/java/tn/esprit/dima_maak/config/SecurityConfig.java
+++ src/main/java/tn/esprit/dima_maak/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
     http
             .csrf(AbstractHttpConfigurer::disable)
-            .securityMatcher("/admin/**")
+            .securityMatcher("/user/**")
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers("/auth/**").permitAll();
                 auth.requestMatchers("/admin/**").hasRole("ADMIN");
