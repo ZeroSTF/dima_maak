49300de816eed635f94959f019b255816cdc2038
zero <35609287+ZeroSTF@users.noreply.github.com>
2024-04-20 17:47:25 +0100

    Revert "Loan module"

--- src/main/java/tn/esprit/dima_maak/entities/Loan.java
+++ src/main/java/tn/esprit/dima_maak/entities/Loan.java
@@ -3,17 +3,10 @@ package tn.esprit.dima_maak.entities;
 import lombok.*;
 import jakarta.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
-
-
 import java.time.LocalDate;
-
-
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-@Setter
 @Entity
 @Getter
 @Setter
@@ -24,18 +17,16 @@ public class Loan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private java.sql.Date date;
+    private LocalDate date;
     private Float amount;
     private Float unpaidAmount;
     private Integer termInMonths;
     private Float interest;
-    private java.sql.Date startDate;
+    private LocalDate startDate;
     @Enumerated(EnumType.STRING)
     private LType purpose;
     @Enumerated(EnumType.STRING)
     private LStatus status;
-    @Enumerated
-    private LMethod PaimentMethod;
     @ManyToOne
     private User user;
     @OneToMany(mappedBy = "loan", cascade = CascadeType.ALL)
--- src/main/java/tn/esprit/dima_maak/entities/Portion.java
+++ src/main/java/tn/esprit/dima_maak/entities/Portion.java
@@ -3,29 +3,23 @@ import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
-@Data
-@Table(name="Tportion")
-
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Tportion")
 public class Portion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private Date date;
-    private double amount;
-    private double Md;
-    private double Inte;
-    private double Amortissement;
-
- 
-    
+    private LocalDate date;
+    private Float amount;
     @Enumerated(EnumType.STRING)
     private PStatus status;
     @ManyToOne
-    @JoinColumn(name = "loan_id")
     private Loan loan;
     @OneToOne(mappedBy = "portion", cascade = CascadeType.ALL)
     private Penalty penalty;
