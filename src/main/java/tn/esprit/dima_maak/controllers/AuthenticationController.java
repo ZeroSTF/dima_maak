416fd7a62b4b0aa24bdd5d02da0f87fe1ee846ff
ZeroSTF <kateskatous@gmail.com>
2024-04-26 12:42:08 +0100

    authentication fixes

--- src/main/java/tn/esprit/dima_maak/controllers/AuthenticationController.java
+++ src/main/java/tn/esprit/dima_maak/controllers/AuthenticationController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.dima_maak.DTO.LoginDTO;
